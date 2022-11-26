a1a6820850d59cbc7746296e0a04bff1d0109875
minnos <elvanarvis@hotmail.com>
2022-11-26 20:52:01 +0300

    Remove dark mode

--- app/src/main/java/com/bootcamp/inviobootcampodev7_todoapp/MainActivity.kt
+++ app/src/main/java/com/bootcamp/inviobootcampodev7_todoapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bootcamp.inviobootcampodev7_todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
